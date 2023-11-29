b19e78e12f22bf0eaa5167d1ff608fd52e51ac7b
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-29 18:31:53 +0100

    still testing POST routes...

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -40,30 +40,60 @@ describe("GET /api/movies/:id", () => {
 
 describe("POST /api/movies", () => {
   it("should create a new movie", async () => {
-    const response = await request(app).post("/api/movies").send({
-      title: "Dr. Strange",
-      director: "Scott Derrickson",
-      year: "2016",
+    const newMovie = {
+      title: "Captain America: The First Avenger",
+      director: "Joe Johnston",
+      year: "2011",
       color: "1",
-      duration: 99,
-    });
-    expect(response.statusCode).toBe(201);
+      duration: 124,
+    };
+
+    const response = await request(app).post("/api/movies").send(newMovie);
+
     expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toEqual({
-      id: expect.any(Number),
-      title: "Dr. Strange",
-      director: "Scott Derrickson",
-      year: "2016",
-      color: "1",
-      duration: 99,
-    });
+
+    expect(response.statusCode).toBe(201);
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("title");
+    expect(response.body).toHaveProperty("director");
+    expect(response.body).toHaveProperty("year");
+    expect(response.body).toHaveProperty("color");
+    expect(response.body).toHaveProperty("duration");
+
+    expect(typeof response.body.id).toBe("number");
+    expect(typeof response.body.title).toBe("string");
+    expect(typeof response.body.director).toBe("string");
+    expect(typeof response.body.year).toBe("string");
+    expect(typeof response.body.color).toBe("string");
+    expect(typeof response.body.duration).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM `movies` WHERE `id` = ?",
+      [response.body.id],
+    );
+
+    const [inserterMovieRetrievedFromDatabase] = result;
+
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("id");
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("title");
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("director");
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("year");
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("color");
+    expect(inserterMovieRetrievedFromDatabase).toHaveProperty("duration");
+
+    expect(inserterMovieRetrievedFromDatabase.title).toStrictEqual(newMovie.title);
+    expect(inserterMovieRetrievedFromDatabase.director).toStrictEqual(newMovie.director);
+    expect(inserterMovieRetrievedFromDatabase.year).toStrictEqual(newMovie.year);
+    expect(inserterMovieRetrievedFromDatabase.color).toStrictEqual(newMovie.color);
+    expect(inserterMovieRetrievedFromDatabase.duration).toStrictEqual(newMovie.duration);
   });
 
   it("should return a 400 error if the movie is invalid", async () => {
     const response = await request(app).post("/api/movies").send({
-      title: "Dr. Strange",
-      director: "Scott Derrickson",
-      year: "2016",
+      title: "Captain America: The First Avenger",
+      director: "Joe Johnston",
+      year: "2011",
       color: "1",
     });
     expect(response.statusCode).toBe(400);
